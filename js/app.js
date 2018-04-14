811f0843f90d1212b2fbe3914cc9874354e88329
Gerben Boersma <geboersma@gmail.com>
2018-04-14 23:55:53 +0200

    Added functions after win and loss, comments

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
  * Create a list that holds all of your cards
  */
 
+// These are all the cards available
 const allCardsArray = [
 	'diamond',
 	'diamond',
@@ -21,6 +22,7 @@ const allCardsArray = [
 	'bomb'
 ];
 
+// An arraay with just all the unique cards
 const uniqueCardsArray = [
 	'diamond',
 	'plane',
@@ -32,6 +34,8 @@ const uniqueCardsArray = [
 	'bomb'
 ];
 
+// object linking the array names for each card
+// with the class names used to display them
 const cardIcons = {
 	diamond: 'fa-diamond',
 	plane: 'fa-paper-plane-o',
@@ -43,25 +47,55 @@ const cardIcons = {
 	bomb: 'fa-bomb'
 };
 
-let activeCards = [];
-let openCards = [];
+// the maximum amount of allowed moves :)
+const startingMoves = 4;
 
+// declare all variables for the game mechanics
+let cards;
+let activeCards;
+let openCards;
+let movesMade;
+let winningSpree;
+
+// these variables are used to disable play while
+// the animations are playing
 let pauseTimer;
-let disablePlay = false;
+let disablePlay;
 
+// save some elements to variables to use later
 const resetButton = document.querySelector('.restart');
+const againButton = document.getElementById('again');
 const deck = document.querySelector('ul.deck');
+const endScreen = document.getElementById('endingScreen');
 
-// empty deck
-deck.innerHTML = '';
+let movesNum = document.querySelector('.moves');
+let movesText = document.querySelector('.movesText');
+let starsContainer = document.querySelector('.stars');
 
+
+/**************************************************************************************
+ ******************************** Game functions **************************************
+ **************************************************************************************
+ */
+
+// reset the game to start again
+// Used for the reset button at the top and for the 'play again' button at the end
 function reset() {
 
-	console.log('reset!');
+	// empty deck at first
+	deck.innerHTML = '';
 
+	// shuffle the deck
 	let shuffledDeck = shuffle(allCardsArray);
-	let deckHtml = '';
+	
+	// reset all starting variables
+	movesMade = 0;
+	winningSpree = 0;
 	activeCards = [];
+	openCards = [];
+	disablePlay = false;
+
+	let deckHtml = '';
 
 	shuffledDeck.forEach(function(elem){
 		let cardClass = cardIcons[elem];
@@ -75,18 +109,35 @@ function reset() {
 
 	deck.innerHTML = deckHtml;
 
-	console.log(activeCards);
+	// now that the deck is shuffled and the cards are
+	// in the DOM, store them in this variable
+	cards = document.querySelectorAll('.card');
 
-	let cards = document.querySelectorAll('.card');
-
+	// add the event listeners to all cards in the game
 	for (var i=0; i<cards.length; i++) {
 
 		cards[i].addEventListener('click', function(){
+			// only allow the function when there is no 
+			// animation playing or defeat
 			if (!disablePlay) {
 				openCard(this);
 			}			
 		});
 	}
+
+	// fill the stars list with correct amount of stars
+	let newStarsHtml = '';
+	
+	for (var i=0; i < startingMoves; i++) {
+		newStarsHtml += '<li><i class="fa fa-star"></i></li>';
+	}
+
+	starsContainer.innerHTML = newStarsHtml;
+
+	// reset amount of moves to be made
+	movesNum.innerHTML = startingMoves;
+	movesText.innerHTML = 'Moves';
+	
 }
 
 reset();
@@ -94,6 +145,192 @@ reset();
 resetButton.addEventListener('click', function(){
 	reset();
 });
+
+
+// this function is called when clicking on a card
+function openCard (elem) {
+
+	elem.classList.add('open', 'show');
+	let innerClass = elem.querySelector('.fa');
+
+	handleOpenCards(innerClass.classList);
+}
+
+
+// check if there are more cards than 1 opened
+function handleOpenCards (classList) {
+
+	let openCardClass = classList.value;
+
+	uniqueCardsArray.forEach(function(elem){
+		let cardClass = cardIcons[elem];
+		if (openCardClass.indexOf(cardClass) != -1) {
+			openCards.push(elem);
+		}
+	});
+
+	// if there are 2 cards open, we need to check if they match
+	if (openCards.length === 2) {
+		matchCards();
+	}
+}
+
+
+// function for matching the cards that are open
+function matchCards () {
+
+	let shownCards = document.querySelectorAll('.card.open.show');
+	let itsamatch;
+
+	// when both open cards are the same
+	// show the player it's a match!
+	if (openCards[0] === openCards[1]) {
+
+		shownCards[0].classList.add('match');
+		shownCards[1].classList.add('match');
+		itsamatch = true;
+
+		// add one, because who knows...
+		winningSpree++;
+
+	} else {
+
+		// there is no match, show this to the player
+		shownCards[0].classList.add('error');
+		shownCards[1].classList.add('error');
+		itsamatch = false;
+
+		// remove a star from the total amount
+		substractOneMove();
+		winningSpree = 0;
+	}
+
+	// disable the cards visibly by adding a class
+	// to be removed again after the timeOut
+	addClassesToAll(cards, 'disabled');
+
+	// update the amount of moves made
+	movesMade++;
+
+	// if the player has 3 correct guesses in a row
+	// reward him by returning 1 lost star
+	if (winningSpree === 3) {
+		addOneMove();
+		winningSpree = 0;
+	}
+
+	disablePlay = true;
+	pauseTimer = setTimeout(function(){
+
+		// deactivate the opened cards
+		removeActiveStatus(shownCards[0], itsamatch);
+		removeActiveStatus(shownCards[1], itsamatch);
+
+		if (activeCards.length === 0) {
+			youWin();
+		}
+
+		// remove disabled class from all cards
+		removeClassesToAll(cards, 'disabled');
+
+		// reset array with open cards
+		openCards = [];
+
+		// enable play
+		disablePlay = false;
+
+		// there are not matches anymore, so this is now false
+		itsamatch = false;
+
+		// remove the timer
+		clearTimeout(pauseTimer);
+
+	}, 1200);
+}
+
+
+// Remove the card from the active game
+function removeActiveStatus (card, matches) {
+
+	card.classList.remove('open', 'show', 'error');
+
+	if (matches && openCards.length > 0) {
+		let index = activeCards.indexOf(openCards[0]);
+		activeCards.splice(index, 1);
+		openCards.splice(0, 1);
+	}
+
+}
+
+
+// Substract 1 from the available moves left
+function substractOneMove () {
+
+	let movesAmount = parseInt(movesNum.innerText);
+	let stars = document.querySelectorAll('.stars > li');
+
+	if (movesAmount > 0) {
+		movesAmount -= 1;
+	}
+
+	if (stars.length > 0) {
+		
+		// set all but the remaining stars to invisible
+		// to maintain their position
+		for (var i=0; i < startingMoves; i++) {
+			if (i+1 > movesAmount) {
+				stars[i].style.visibility = 'hidden';
+			}
+		}
+	}
+
+	// Subtract 1 from the number shown
+	movesNum.innerText = movesAmount;
+
+	// and correct the wording accordingly
+	if (movesAmount === 1) {
+		movesText.innerText = 'Move';
+	} else {
+		movesText.innerText = 'Moves';
+	}
+
+	// if we've reached 0 moves available, well that's sad...
+	// the player lost, so disable play
+	if (movesAmount === 0) {
+
+		disablePlay = true;
+
+		// wait a bit before confronting the player with their defeat
+		pauseTimer = setTimeout(function(){
+			youLose();
+			clearTimeout(pauseTimer);
+		}, 2000);
+	}
+}
+
+
+// Add 1 move to the available moves left
+function addOneMove () {
+
+	let movesAmount = parseInt(movesNum.innerText);
+	let stars = document.querySelectorAll('.stars > li');
+
+	movesNum.innerText = movesAmount + 1;
+
+	// loop through stars until finding one with the visibility style
+	for (var i=0; i < stars.length; i++) {
+		if (stars[i].hasAttribute('style')) {
+			// then remove this attribute and break out of loop
+			stars[i].removeAttribute('style');
+			break;
+		}
+	}
+}
+
+/**************************************************************************************
+ ************************** Supporting functions **************************************
+ **************************************************************************************
+ */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -110,73 +347,69 @@ function shuffle(array) {
     return array;
 }
 
-function openCard (elem) {
-	console.log(elem);
 
-	elem.classList.add('open', 'show');
-	let innerClass = elem.querySelector('.fa');
+// Function to add classes to a selection of elements at once
+function addClassesToAll(items, classname) {
 
-	handleOpenCards(innerClass.classList);
-}
-
-function handleOpenCards (classList) {
-
-	let openCardClass = classList.value;
-
-	uniqueCardsArray.forEach(function(elem){
-		let cardClass = cardIcons[elem];
-		if (openCardClass.indexOf(cardClass) != -1) {
-			openCards.push(elem);
-		}
-	});
-
-	if (openCards.length === 2) {
-		matchCards();
+	for (var i=0; i < items.length; i++) {
+		items[i].classList.add(classname);
 	}
 }
 
-function matchCards () {
 
-	let shownCards = document.querySelectorAll('.card.open.show');
-	let itsamatch = false;
+// Function to remove classes from a selection of elements at once
+function removeClassesToAll(items, classname) {
 
-	if (openCards[0] === openCards[1]) {
-		shownCards[0].classList.add('match');
-		shownCards[1].classList.add('match');
-		itsamatch = true;
+	for (var i=0; i < items.length; i++) {
+		items[i].classList.remove(classname);
+	}
+}
+
+
+/**************************************************************************************
+ **************************** End game functions **************************************
+ **************************************************************************************
+ */
+
+function youLose () {
+
+	endScreen.style.display = 'block';
+	setTimeout(function(){
+		endScreen.classList.add('show');
+		endScreen.querySelector('h2').innerText = 'You lost the game!';
+		endScreen.querySelector('p').innerText = 'You spend ' + movesMade + ' moves on it.';
+	}, 200);
+}
+
+function youWin () {
+
+	let movesEndText = '';
+	let movesAmount = parseInt(movesNum.innerText);
+	if (movesAmount > 1) {
+		movesEndText = 'stars';
 	} else {
-		shownCards[0].classList.add('error');
-		shownCards[1].classList.add('error');
+		movesEndText = 'star';
 	}
 
-	disablePlay = true;
-	pauseTimer = setTimeout(function(){
-
-		removeActiveStatus(shownCards[0], itsamatch);
-
-		if (activeCards.length === 0) {
-			console.log('you won!');
-		}
-
-		openCards = [];
-		disablePlay = false;
-
-	}, 1200);
+	endScreen.style.display = 'block';
+	setTimeout(function(){
+		endScreen.classList.add('show');
+		endScreen.querySelector('h2').innerText = 'Congratulations! You won!';
+		endScreen.querySelector('p').innerText = 'With ' + movesMade + ' moves and ' + movesAmount + ' ' + movesEndText + '!';
+	}, 200);
 }
 
-function removeActiveStatus (card, matches) {
+againButton.addEventListener('click', function() {
 
-	card.classList.remove('open', 'show', 'error');
+	reset();	
 
-	if (matches) {
-		openCards.forEach(function(item) {
-			let index = activeCards.indexOf(item);
-			activeCards.splice(index, 1);
-		});
-	}
-
-	console.log(activeCards);
-}
+	endScreen.classList.remove('show');
+	setTimeout(function(){
+		endScreen.style.display = 'none';
+		endScreen.querySelector('h2').innerText = '';
+		endScreen.querySelector('p').innerText = '';
+	}, 500);
+});
 
 /*
  * set up the event listener for a card. If a card is clicked:
